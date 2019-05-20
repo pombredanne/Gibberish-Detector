37cc6a8018251c8f07af6dda904a338ded4393f3
Haolin Wang <shakejameswang@outlook.com>
2019-05-20 15:11:33 -0400

    ok

--- gib_detect.py
+++ gib_detect.py
@@ -4,12 +4,17 @@ import pickle
 import gib_detect_train
 
 model_data = pickle.load(open('gib_model.pki', 'rb'))
+f = open("default.txt", "r+")
+default_words = f.read().splitlines()
 
 while True:
     l = input()
     model_mat = model_data['mat']
     threshold = model_data['thresh']
 
-    print(gib_detect_train.avg_transition_prob(l, model_mat))
-    print(threshold)
-    print(gib_detect_train.avg_transition_prob(l, model_mat) > threshold)
+    if l.lower() in default_words:
+        print(True)
+    else:
+        # print(gib_detect_train.avg_transition_prob(l, model_mat))
+        # print(threshold)
+        print(gib_detect_train.avg_transition_prob(l, model_mat) > threshold)
