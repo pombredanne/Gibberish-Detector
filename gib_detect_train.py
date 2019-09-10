e4f02e3cb6e37ecd2b2c80fc692477b6eba7ee70
Amit Chaudhary <meamitkc@gmail.com>
2019-09-10 14:36:33 +0545

    Port the model to Python 3

--- gib_detect_train.py
+++ gib_detect_train.py
@@ -7,17 +7,20 @@ accepted_chars = 'abcdefghijklmnopqrstuvwxyz '
 
 pos = dict([(char, idx) for idx, char in enumerate(accepted_chars)])
 
+
 def normalize(line):
     """ Return only the subset of chars from accepted_chars.
     This helps keep the  model relatively small by ignoring punctuation, 
-    infrequenty symbols, etc. """
+    infrequently symbols, etc. """
     return [c.lower() for c in line if c.lower() in accepted_chars]
+
 
 def ngram(n, l):
     """ Return all n grams from l after normalizing """
     filtered = normalize(l)
     for start in range(0, len(filtered) - n + 1):
         yield ''.join(filtered[start:start + n])
+
 
 def train():
     """ Write a simple model as a pickle file """
@@ -26,7 +29,7 @@ def train():
     # prior or smoothing factor.  This way, if we see a character transition
     # live that we've never observed in the past, we won't assume the entire
     # string has 0 probability.
-    counts = [[10 for i in xrange(k)] for i in xrange(k)]
+    counts = [[10 for i in range(k)] for i in range(k)]
 
     # Count transitions from big text file, taken 
     # from http://norvig.com/spell-correct.html
@@ -41,10 +44,10 @@ def train():
     # http://squarecog.wordpress.com/2009/01/10/dealing-with-underflow-in-joint-probability-calculations/
     for i, row in enumerate(counts):
         s = float(sum(row))
-        for j in xrange(len(row)):
+        for j in range(len(row)):
             row[j] = math.log(row[j] / s)
 
-    # Find the probability of generating a few arbitrarily choosen good and
+    # Find the probability of generating a few arbitrarily chosen good and
     # bad phrases.
     good_probs = [avg_transition_prob(l, counts) for l in open('good.txt')]
     bad_probs = [avg_transition_prob(l, counts) for l in open('bad.txt')]
@@ -56,6 +59,7 @@ def train():
     thresh = (min(good_probs) + max(bad_probs)) / 2
     pickle.dump({'mat': counts, 'thresh': thresh}, open('gib_model.pki', 'wb'))
 
+
 def avg_transition_prob(l, log_prob_mat):
     """ Return the average transition prob from l through log_prob_mat. """
     log_prob = 0.0
@@ -66,10 +70,6 @@ def avg_transition_prob(l, log_prob_mat):
     # The exponentiation translates from log probs to probs.
     return math.exp(log_prob / (transition_ct or 1))
 
+
 if __name__ == '__main__':
     train()
-
-
-
-    
-    
